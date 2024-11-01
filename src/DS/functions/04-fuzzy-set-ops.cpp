47d00d77b6e7fe319826c359ae8bc6a60c4865b2
Surab Parajuli <87257970+WoodenNebula@users.noreply.github.com>
2024-11-01 22:30:13 +0545

    feat: complete Q4 of 2nd chapter

---
+++ src/DS/functions/04-fuzzy-set-ops.cpp
@@ -0,0 +1,46 @@
+#include "../set-theory/set-helper.h"
+
+namespace FUZZY_SET
+{
+  FuzzySet Union(const FuzzySet& setA, const FuzzySet& setB) {
+    FuzzySet setU(setA);
+
+    for (size_t i = 0; i < setA.size(); i++) {
+      setU[i] = std::max(setA[i], setB[i]);
+    }
+
+    return setU;
+  }
+  FuzzySet Intersection(const FuzzySet& setA, const FuzzySet& setB) {
+    FuzzySet setN(setA);
+
+    for (size_t i = 0; i < setA.size(); i++) {
+      setN[i] = std::min(setA[i], setB[i]);
+    }
+
+    return setN;
+  }
+  FuzzySet Complement(const FuzzySet& set) {
+    FuzzySet setC(set);
+
+    for (size_t i = 0; i < setC.size(); i++) {
+      setC[i] = 1 - setC[i];
+    }
+
+    return setC;
+  }
+}
+
+int main() {
+  using namespace FUZZY_SET;
+
+  FuzzySet A = InputSet(0, 'A');
+  FuzzySet B = InputSet(A.size(), 'B');
+
+  std::cout << "\nFuzzy Set A = " << A << std::endl;
+  std::cout << "Fuzzy Set B = " << B << std::endl;
+
+  std::cout << "\nA u B = " << Union(A, B) << std::endl;
+  std::cout << "A n B = " << Intersection(A, B) << std::endl;
+  std::cout << "A\' = " << Complement(A) << std::endl;
+}
--- src/DS/set-theory/set-helper.h
+++ src/DS/set-theory/set-helper.h
@@ -45,3 +45,98 @@ namespace SET
     return _set;
   }
 };
+
+namespace FUZZY_SET
+{
+  using FuzzySet = std::vector<float>;
+
+  FuzzySet InputSet(size_t count, char set = 'A') {
+    std::cout << "For membership set" << set << ":\n";
+    std::cout << "n(" << set << ") = ";
+
+    if (count == 0) {
+      std::cin >> count;
+    }
+    else {
+      std::cout << count << std::endl;
+    }
+
+    FuzzySet _set(count, 0);
+
+    double x = 0;
+    for (size_t i = 0; i < count; i++) {
+      std::cin >> x;
+
+      _set[i] = x;
+    }
+    return _set;
+  }
+}
+
+// namespace FUZZY_SET
+// {
+//   struct Element {
+//     int a;
+//     float u;
+
+//     Element() {}
+
+//     Element(int a, float u) :a(a), u(u) {}
+
+//     friend std::ostream& operator<<(std::ostream& out, const Element& e) {
+//       out << "(" << e.a << ", " << e.u << ")";
+//       return out;
+//     }
+
+//     friend std::istream& operator>>(std::istream& in, Element& e) {
+//       std::cout << "a = ";
+//       in >> e.a;
+//       std::cout << "u = ";
+//       in >> e.u;
+//       std::cout << std::endl;
+//       return in;
+//     }
+
+
+//     friend bool operator<(const Element& a, const Element& b) {
+//       if (a.a == b.a) {
+//         return a.u < b.u;
+//       }
+//       else {
+//         return (a.a < b.a);
+//       }
+//     }
+
+//     friend bool operator==(Element a, Element b) {
+//       return (a.a == b.a) && (a.u == b.u);
+//     }
+//   };
+
+//   using FuzzySet = std::vector<Element>;
+
+//   FuzzySet InputSet(char set = 'A') {
+//     FuzzySet _set;
+//     std::cout << "n(" << set << ") = ";
+//     size_t count = 0;
+//     std::cin >> count;
+
+//     Element e;
+
+//     for (size_t i = 0; i < count; i++) {
+//       bool isDuplicate = false;
+
+//       do {
+//         std::cin >> e;
+//         isDuplicate = std::find(_set.begin(), _set.end(), e) != _set.end();
+
+//         if (isDuplicate) {
+//           std::cerr << "Element must be unique:\n";
+//         }
+//       } while (isDuplicate);
+
+//       _set.push_back(e);
+//     }
+//     std::sort(_set.begin(), _set.end());
+//     return _set;
+//   }
+// };
