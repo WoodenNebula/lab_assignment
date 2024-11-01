9f5fcf8d60188fc4ad6862983cc7e1fcff088d22
Surab Parajuli <87257970+WoodenNebula@users.noreply.github.com>
2024-11-01 19:12:10 +0545

    feat: start second chapter: complete first question

---
+++ src/DS/functions/01-composite.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int f(int x) {
+  return 2 * x + 3;
+}
+
+int g(int x) {
+  return 3 * x + 2;
+}
+
+int composite(int f(int), int g(int), int x) {
+  return f(g(x));
+}
+
+int main() {
+  int x = 0;
+  std::cout << "Enter x: ";
+  std::cin >> x;
+
+  std::cout << "fog = " << composite(f, g, x) << std::endl;
+  std::cout << "gof = " << composite(g, f, x) << std::endl;
+}
