1af5151d6a810127a77da97051c1f37bcef44cd6
Surab Parajuli <87257970+WoodenNebula@users.noreply.github.com>
2024-11-01 22:06:21 +0545

    bugFix: address a edge case scenario for printing 'Set' and rename Set methods and, type

--- src/DS/set-theory/set-helper.h
+++ src/DS/set-theory/set-helper.h
@@ -2,40 +2,46 @@
 #include <vector>
 #include <algorithm>
 
+template <typename T>
+std::ostream& operator <<(std::ostream& out, const std::vector<T>& set) {
+  out << "{";
+  if (set.size() > 0) {
+    for (size_t i = 0; i < set.size() - 1; i++) {
+      out << set[i] << ", ";
+    }
+    out << set[set.size() - 1] << "}";
+  }
+  else {
+    out << "NullSet}";
+  }
+
+  return out;
+}
+
 namespace SET
 {
-  std::vector<int> inputSet(char set = 'A') {
-    std::vector<int> _set;
+  using Set = std::vector<int>;
+
+  Set InputSet(char set = 'A') {
+    Set _set;
     std::cout << "n(" << set << ") = ";
     size_t count = 0;
     std::cin >> count;
 
     int x = 0;
     for (size_t i = 0; i < count; i++) {
-      bool match = false;
+      bool isDuplicate = false;
       do {
         std::cin >> x;
-        match = std::find(_set.begin(), _set.end(), x) != _set.end();
-        if (match) {
+        isDuplicate = std::find(_set.begin(), _set.end(), x) != _set.end();
+        if (isDuplicate) {
           std::cerr << "Element must be unique:";
         }
-      } while (match);
+      } while (isDuplicate);
 
       _set.push_back(x);
     }
     std::sort(_set.begin(), _set.end());
     return _set;
   }
-
-
 };
-template <typename T>
-std::ostream& operator <<(std::ostream& out, const std::vector<T>& set) {
-  out << "{";
-  for (size_t i = 0; i < set.size() - 1; i++) {
-    out << set[i] << ", ";
-  }
-  out << set[set.size() - 1] << "}";
-
-  return out;
-}
