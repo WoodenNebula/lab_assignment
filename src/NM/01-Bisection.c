3a47da6d62c53fa0dcccd4b9eee8b1bf338ec7de
WoodenNebula <87257970+WoodenNebula@users.noreply.github.com>
2025-04-14 17:55:24 +0545

    NM: complete Q1

---
+++ src/NM/01-Bisection.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<math.h>
+
+#include "commons.h"
+
+#define E 0.01
+
+int main() {
+    Coeff coeff;
+    promptEquation(&coeff);
+    // printEquation(&coeff);
+
+    float x_lower, x_upper;
+    promptTwoGuess(&x_lower, &x_upper);
+
+    float f_lower = f(coeff, x_lower);
+    float f_upper = f(coeff, x_upper);
+
+    float x_mid = (x_lower + x_upper) / 2;
+    float f_mid = f(coeff, x_mid);
+
+
+    if (f_lower * f_upper > 0) {
+        printf("%.3f and %.3f do not bracket root\n", x_lower, x_upper);
+        footer();
+        return 0;
+    }
+
+
+    float root;
+
+    int itr = 1;
+    while (1) {
+        f_lower = f(coeff, x_lower);
+        f_upper = f(coeff, x_upper);
+
+        x_mid = (x_lower + x_upper) / 2;
+        f_mid = f(coeff, x_mid);
+
+        float prod = f_lower * f_mid;
+
+        // trace(itr, x_lower, x_upper, x_mid, f_lower, f_upper, f_mid, prod);
+        if (prod == 0) {
+            root = x_mid;
+            break;
+        }
+        else if (prod < 0) { x_upper = x_mid; }
+        else { x_lower = x_mid; }
+
+        float e = fabs((x_lower - x_upper) / x_upper);
+
+        if (e < E) {
+            root = x_mid;
+            break;
+        }
+        itr++;
+    }
+    printf("Root=%.3f\n", root);
+
+    footer();
+    return 0;
+}
