744ac5173ef49bff1774115fe935e839289445b6
noxtar <87257970+WoodenNebula@users.noreply.github.com>
2025-08-03 22:32:53 +0545

    [OS] update run script

--- src/OS/syscalls/run.sh
+++ src/OS/syscalls/run.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 fname="$1"
-fdir="./src/OS/syscalls/"
+fdir="./src/OS"
 bin="./src/OS/outputs/"
 
 if [ $# = 0 ]; then
   echo "File name required!!!"
 else
+  if [ $# = 2 ]; then
+    fdir="$fdir/threads/"
+  else
+    fdir="$fdir/syscalls/"
+  fi
+
   fbin="$bin$1"
   file="$fdir$1.c"
   # echo "file=$file, fbin=$fbin"
