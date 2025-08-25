9228c59cd90f50d27af4336a7a1b086c9a1a3a5b
noxtar <87257970+WoodenNebula@users.noreply.github.com>
2025-08-26 00:46:32 +0545

    [OS] further extend run script to take env var SRC_DIR for switching on subdir

--- src/OS/syscalls/run.sh
+++ src/OS/syscalls/run.sh
@@ -6,17 +6,16 @@ bin="./src/OS/outputs/"
 if [ $# = 0 ]; then
   echo "File name required!!!"
 else
-  if [ $# = 2 ]; then
-    fdir="$fdir/threads/"
-  else
-    fdir="$fdir/syscalls/"
+  if [ -n "$SRC_DIR" ]; then
+    fdir="$fdir/$SRC_DIR/"
+    fbin="$bin$1"
+    file="$fdir$1.c"
+    # echo "file=$file, fbin=$fbin"
+    gcc $file -o $fbin
+    chmod u+x $fbin
+    $fbin
+    echo
+  else 
+    echo "SRC_DIR environment variable not set, please set it to the sub directory of OS/ from where the .c file is sourced"
   fi
-
-  fbin="$bin$1"
-  file="$fdir$1.c"
-  # echo "file=$file, fbin=$fbin"
-  gcc $file -o $fbin
-  chmod u+x $fbin
-  $fbin
-  echo
 fi
