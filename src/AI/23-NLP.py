2af4b33d8202dea1c1f0a2af889daf1544e612d1
Surab Parajuli <87257970+WoodenNebula@users.noreply.github.com>
2025-08-28 01:16:27 +0545

    [AI]: add missing download command to nlp

--- src/AI/23-NLP.py
+++ src/AI/23-NLP.py
@@ -22,6 +22,10 @@ from nltk.stem import PorterStemmer
 
 def main() -> None:
     nltk.download('wordnet')
+    nltk.download('punkt_tab')
+    nltk.download('stopwords')
+    nltk.download('averaged_perceptron_tagger_eng')
+    
     text = ("Natural Language Processing (NLP) is a field of Artificial Intelligence. "
             "It enables machines to understand and interpret human language.")
 
