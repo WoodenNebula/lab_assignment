bad718243c6554f97b3851b2ce9665b36747d83a
noxtar <87257970+WoodenNebula@users.noreply.github.com>
2025-08-29 22:54:47 +0545

    update run script to be even more flexible, prefixed name with full file path now allowed

--- src/run.sh
+++ src/run.sh
@@ -1,29 +1,77 @@
 #!/bin/bash
 file="$1"
-bin="./bin"
+src=""
+bin=""
+fileName=""
 
-if [ $# = 0 ]; then
-  echo "File path required!!!"
-else
-  # setup output folder
-  fdir=$(dirname "$file")
-  fbinPath="$bin/${fdir#./src/}"
-  mkdir -p "$fbinPath"
 
-    ext=${file##*.} # get the extension
-    filePathNoExt=${file%.*}
-    filePathNoExtNoSrc=${filePathNoExt#./src/}
-    fbin="$bin/$filePathNoExtNoSrc"
+make_src_path() {
+  local inputFile=$1
 
+  if [[ "$inputFile" = *.* ]]; then
+    src="$inputFile"
+    fileName="$(basename -a $inputFile)"
+  else
+    fileNamePrefix="$(basename "$inputFile")"
+    fileDir="$(dirname "$inputFile")"
+    fileName="$(ls "$fileDir" | grep "${fileNamePrefix}" )"
+    
+    if [[ -z "$fileName" ]]; then
+      echo "File '$input' doesn't exist"
+      exit 1
+    fi
+
+    src="$fileDir/$fileName"
+  fi
+}
+
+compile_and_run() {
+    local src="$1"
+    local bin="$2"
+    local isC=$3
 
     # compile c/c++ file
-    if [[ "$ext" == "c" ]]; then
-      bear -- gcc -g -I. $file -o $fbin
+    if $isC; then
+      bear -- gcc -g -I. $src -o $bin
     else
-      bear -- g++ -g -I. $file -o $fbin
+      bear -- g++ -g -I. $src -o $bin
     fi
     # run
-    chmod u+x $fbin
-    $fbin
+    chmod u+x $bin
+    $bin
     echo
-fi
+}             
+
+run()  {
+  if [ $# = 0 ]; then
+    echo "File path required!!!"
+  else
+    file=${file#./}
+    make_src_path "$file"
+    
+    # extract extension
+    ext="${src##*.}"
+
+    # make output folder
+    fdir="$(dirname "$src")"
+
+    binDir="${fdir#src/}"
+    binDir="./bin/$binDir"
+
+    bin="$binDir/$fileName"
+
+    mkdir -p "$binDir"
+
+    # file type
+    isC=false
+    if [[ "$ext" == "c" ]]; then
+      isC=true
+    else 
+      isC=false
+    fi
+
+    compile_and_run $src $bin $isC
+  fi
+}
+
+run $file
