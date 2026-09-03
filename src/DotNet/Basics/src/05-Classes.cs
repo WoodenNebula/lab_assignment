a84321116c9b672b955074cc30e3dcec3e4793f7
WoodenNebula <87257970+WoodenNebula@users.noreply.github.com>
2026-09-03 09:50:10 +0545

    [.Net]: complete Q5-6

---
+++ src/DotNet/Basics/src/05-Classes.cs
@@ -0,0 +1,53 @@
+﻿using Basics;
+
+namespace Basics {
+    internal class Employee {
+
+        public Employee(string Name) { Console.WriteLine("<Base Constructor invoked>"); this.ID = -1; this.Name = Name; }
+        public virtual void IncrementID() { Console.WriteLine("<Base IncrementID method invoked>"); this.ID++; }
+        public void PrintID() { Console.WriteLine("<Base PrintID method invoked>"); Console.WriteLine($"[{this.Name}]: " + this.ID); }
+
+        protected int ID;
+        protected string Name;
+    }
+
+    internal class Engineer : Employee {
+        public Engineer(string Name) : base(Name) { base.ID = 10; base.PrintID(); }
+
+        public override void IncrementID() {
+            Console.WriteLine($"[{this.Name}]: Previous ID: " + base.ID);
+            base.IncrementID();
+            Console.WriteLine($"[{this.Name}]: After increment: " + base.ID);
+        }
+    }
+
+
+    internal class Doctor : Employee {
+        public Doctor(string Name) : base(Name) { base.ID = 20; base.PrintID(); }
+
+        public void Register() { Console.WriteLine($"[{this.Name}]: Registered with ID: " + this.ID); }
+
+        public override void IncrementID() {
+            Console.WriteLine($"[{this.Name}]: Previous ID: " + base.ID);
+            base.IncrementID();
+            Console.WriteLine($"[{this.Name}]: After increment: " + base.ID);
+        }
+    }
+}
+
+namespace Assignments {
+    internal class Assignment5 : AssignmentBase {
+
+        public Assignment5() : base("Base Class Action") { }
+
+        public override void Body() {
+
+            Employee engineer = new Engineer("Engineer");
+            engineer.IncrementID();
+
+            Doctor doctor = new Doctor("Doctor");
+            doctor.IncrementID();
+            doctor.Register();
+        }
+    }
+}
---
+++ src/DotNet/Basics/src/06-Polymorphism.cs
@@ -0,0 +1,42 @@
+﻿using Basics;
+
+namespace Basics {
+    internal class Teacher : Employee {
+        public Teacher(string Name) : base(Name) { base.ID = 60; base.PrintID(); }
+
+        public override void IncrementID() {
+            Console.WriteLine($"[{this.Name}]: <Child IncrementID method invoked>");
+            base.IncrementID();
+        }
+
+        public new void PrintID() {
+            Console.WriteLine($"[{this.Name}]: <Child PrintID method invoked>");
+        }
+    }
+}
+
+namespace Assignments {
+    internal class Assignment6 : AssignmentBase {
+
+        public Assignment6() : base("Polymorphism") { }
+
+        public override void Body() {
+
+            Teacher teacher = new Teacher("Teacher");
+            Employee employee = new Teacher("Teach");
+
+            Console.WriteLine();
+            Console.WriteLine("---Method Overriding---");
+            teacher.IncrementID();
+
+            Console.WriteLine();
+
+            Console.WriteLine("---Method Hiding---");
+            Console.WriteLine("Method from parent ref:");
+            employee.PrintID();
+            Console.WriteLine("Method from child ref:");
+            teacher.PrintID();
+        }
+    }
+}
+
