08d9cfda6d36cc3f9dcf2ae8050b4ddc563c03d2
WoodenNebula <87257970+WoodenNebula@users.noreply.github.com>
2026-09-03 22:55:04 +0545

    [.Net]: complete Q10-11

---
+++ src/DotNet/Basics/src/10-Collections.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+
+namespace Assignments {
+    class Assignment10 : AssignmentBase {
+        public Assignment10() : base("Collections") { }
+        public override void Body() {
+            Console.WriteLine("---NonGeneric Collection---");
+            ArrayList nonGenericList = new ArrayList();
+
+            // everything is wrapped and stored as object
+            nonGenericList.Add(108);
+            // reference type is stored directly as reference
+            nonGenericList.Add("Warrior");
+            nonGenericList.Add('A');
+
+            foreach (object item in nonGenericList) {
+                if (item is int) {
+                    // unboxing/casting
+                    int number = (int)item;
+                    Console.WriteLine($" Integer (Unboxed): {number}");
+                }
+                else {
+                    Console.WriteLine($" Object (Reference): {item}");
+                }
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("---Generic Collection---");
+
+            List<int> genericList = new List<int>() { 100, 200, 300 };
+            genericList.Add(400);
+
+            Console.WriteLine($"[ {string.Join(", ", genericList)} ]");
+        }
+    }
+}
---
+++ src/DotNet/Basics/src/11-Template.cs
@@ -0,0 +1,39 @@
+﻿using Basics;
+
+namespace Basics {
+    internal class GenericClass<T> {
+        private T m_genericData;
+
+        public GenericClass(T data) {
+            m_genericData = data;
+            Console.WriteLine($"Instantiated with type '{typeof(T)}'");
+        }
+
+        public T Prop {
+            get => m_genericData;
+            set => m_genericData = value;
+        }
+
+        public string Concat<U>(U valA, U valB) {
+            return $"{valA?.ToString()}{valB?.ToString()}";
+        }
+    }
+}
+
+namespace Assignments {
+    class Assignment11 : AssignmentBase {
+        public Assignment11() : base("Templates") { }
+        public override void Body() {
+            GenericClass<int> intClass = new GenericClass<int>(69);
+            Console.WriteLine($"Property = {intClass.Prop}");
+            Console.WriteLine($"Method as string = '{intClass.Concat("foo", "bar")}'");
+            Console.WriteLine($"Method as int = '{intClass.Concat(6, 9)}'");
+
+            Console.WriteLine();
+            GenericClass<string> stringClass = new GenericClass<string>("Generic String");
+            Console.WriteLine($"Property = {stringClass.Prop}");
+            Console.WriteLine($"Method as string = '{stringClass.Concat("foo", "bar")}'");
+            Console.WriteLine($"Method as int = '{stringClass.Concat(6, 9)}'");
+        }
+    }
+}
