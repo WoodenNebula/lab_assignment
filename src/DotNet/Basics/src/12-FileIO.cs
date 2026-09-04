85ed0e889572e468f91b64ff5b472f1afbd8ffce
WoodenNebula <87257970+WoodenNebula@users.noreply.github.com>
2026-09-04 09:55:34 +0545

    [.Net]: complete Q12

---
+++ src/DotNet/Basics/src/12-FileIO.cs
@@ -0,0 +1,18 @@
+﻿namespace Assignments {
+    class Assignment12 : AssignmentBase {
+        public Assignment12() : base("File IO") { }
+        public override void Body() {
+            string filePath = "12-fileIO.txt";
+            Console.WriteLine("Enter text to write to " + filePath + " ( enter `:q` to stop)");
+
+            StreamWriter writer = new(filePath);
+            string? lineInput;
+            while ((lineInput = Console.ReadLine()) != null && lineInput != ":q") {
+                writer.WriteLine(lineInput);
+            }
+
+            writer.Flush();
+            writer.Close();
+        }
+    }
+}
